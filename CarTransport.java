560e14b64b15ad534d9479363fa352d836520ef9
Aron Larsson <arlars03@gmail.com>
2023-11-13 15:07:20 +0100

    Create class for  car transport

---
+++ CarTransport.java
@@ -0,0 +1,34 @@
+import java.awt.*;
+
+public class CarTransport extends PlatformVehicle {
+
+    private boolean platformIsClosed;
+    public CarTransport(int nrDoors, double enginePower, Color color, String modelName) {
+        super(nrDoors, enginePower, color, modelName);
+    }
+
+    @Override
+    protected boolean platformClosed() {
+        return platformIsClosed;
+    }
+
+    @Override
+    protected void incrementSpeed(double amount) {
+        if (platformClosed()) {
+            // Car can only drive if platform is closed
+            setCurrentSpeed(Math.min(getCurrentSpeed() + speedFactor() * amount, getEnginePower()));
+        }
+    }
+
+    @Override
+    protected void decreaseSpeed(double amount) {
+        setCurrentSpeed(Math.max(getCurrentSpeed() - speedFactor() * amount, 0));
+    }
+
+    private double speedFactor() {
+        return getEnginePower() * 0.01;
+    }
+
+
+}
+
